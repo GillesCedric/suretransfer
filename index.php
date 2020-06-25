5aa5c9c253c7a5e4fe4fa6f76ca76a20daa4f8b4
Gilles Cédric <cedric.nguefack@tapmeppe.com>
2020-06-25 17:31:49 +0100

    modification de index.php

--- index.php
+++ index.php
@@ -29,16 +29,6 @@ include('class/config.php');
 	<div id="main">
 		<div class="container my-4">
 
-			<p class="font-weight-bold">To set or to stop autoplay in Bootstrap carousel you can use data attributes or JavaScript code.</p>
-
-			<p><strong>Detailed documentation and more examples of Bootstrap grid you can find in our <a href="https://mdbootstrap.com/docs/jquery/javascript/carousel/" target="_blank">Bootstrap Carousel Docs</a>.</p>
-
-			<p><strong class="font-weigt-bold">Note: </strong>Autoplay for the carousel is turned on from default.</p>
-
-			<hr class="my-4">
-
-			<h2 class="my-5 h2">Basic carousel - autoplay turned on from default</h2>
-
 			<div id="carouselExample1" class="carousel slide z-depth-1-half" data-ride="carousel">
 				<div class="carousel-inner">
 					<div class="carousel-item active">
